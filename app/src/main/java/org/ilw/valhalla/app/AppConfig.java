4b327e228004b7d7339bad25365ef228c9548fb3
Ilja Winokurow <urodina@tut.by>
2017-01-02 11:09:53 +0100

    Initial commit

---
+++ app/src/main/java/org/ilw/valhalla/app/AppConfig.java
@@ -0,0 +1,9 @@
+package org.ilw.valhalla.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.203.1:8080/evolution/loginservice";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.203.1:8080/evolution/registrationservice";
+}
